c2d2a3d903bcb044b281f021c7b7f4e3ec28465b
Jared Flowers <jared.t.flowers@gmail.com>
2023-08-08 15:49:54 -0400

    adding rop heuristic

---
+++ src/ompl/base/objectives/ROPPathLengthOptimizationObjective.h
@@ -0,0 +1,104 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2010, Rice University
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Rice University nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Luis G. Torres, Jonathan Gammell (allocInformedStateSampler) */
+
+#ifndef OMPL_BASE_OBJECTIVES_ROP_PATH_LENGTH_OPTIMIZATION_OBJECTIVE_
+#define OMPL_BASE_OBJECTIVES_ROP_PATH_LENGTH_OPTIMIZATION_OBJECTIVE_
+
+#include "ompl/base/OptimizationObjective.h"
+#include <Eigen/Core>
+#include <ros/ros.h>
+#include <unsupported/Eigen/CXX11/Tensor>
+#include <geometry_msgs/PoseArray.h>
+#include <sensor_msgs/PointCloud.h>
+#include <rosdyn_core/primitives.h>
+#include <rop_msgs/opv_array_msg.h>
+#include <rop_msgs/opv_msg.h>
+
+namespace ompl
+{
+    namespace base
+    {
+        /** \brief An optimization objective which corresponds to optimizing path length. */
+        class ROPPathLengthOptimizationObjective : public OptimizationObjective
+        {
+        public:
+            ROPPathLengthOptimizationObjective(const SpaceInformationPtr &si);
+
+            /** \brief Returns identity cost. */
+            Cost stateCost(const State *s) const override;
+
+            /** \brief Motion cost for this objective is defined as
+                the configuration space distance between \e s1 and \e
+                s2, using the method SpaceInformation::distance(). */
+            Cost motionCost(const State *s1, const State *s2) const override;
+
+            /** \brief the motion cost heuristic for this objective is
+                simply the configuration space distance between \e s1
+                and \e s2, since this is the optimal cost between any
+                two states assuming no obstacles. */
+            Cost motionCostHeuristic(const State *s1, const State *s2) const override;
+
+            /** \brief Allocate a state sampler for the path-length objective (i.e., direct ellipsoidal sampling). */
+            InformedSamplerPtr allocInformedStateSampler(const ProblemDefinitionPtr &probDefn,
+                                                         unsigned int maxNumberCalls) const override;
+
+                
+            Eigen::Tensor<double,3> m_occupancy;
+            Eigen::Vector3d workspace_lb={-1,-1,0.5};
+            Eigen::Vector3d workspace_ub={1,1,2.5};
+            std::vector<int> m_npnt;
+            rosdyn::ChainPtr chain;
+            std::vector<std::string> link_names;
+            std::vector<Eigen::Affine3d,Eigen::aligned_allocator<Eigen::Affine3d>> m_transformations;
+            std::map<std::string,std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> >> m_test_points;
+            std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > m_points;
+            std::vector<std::string> links_to_check;
+            double step_;
+            double m_resolution=0.05;
+            ros::Subscriber sub_opvs;
+            ros::NodeHandle nh_;
+            double nu_;
+            void WorkcellGrid(void);
+            void opvs_callback(const rop_planning::opv_array_msg::ConstPtr& msg);
+            double totalROP(const std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> >& points);
+            double occupiedROPMultiplier(const Eigen::VectorXd &q);
+            double rop_cost(const Eigen::VectorXd& parent, const Eigen::VectorXd& new_node);
+            double m_inv_resolution;
+        };
+    }
+}
+
+#endif
---
+++ src/ompl/base/objectives/src/ROPPathLengthOptimizationObjective.cpp
@@ -0,0 +1,360 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2010, Rice University
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Rice University nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Jared Flowers */
+
+#include "ompl/base/objectives/ROPPathLengthOptimizationObjective.h"
+#include <memory>
+#include "ompl/base/samplers/informed/PathLengthDirectInfSampler.h"
+
+ompl::base::ROPPathLengthOptimizationObjective::ROPPathLengthOptimizationObjective(const SpaceInformationPtr &si)
+  : ompl::base::OptimizationObjective(si)
+{
+    description_ = "Path Length";
+
+    // Setup a default cost-to-go heuristics:
+    setCostToGoHeuristic(base::goalRegionCostToGo);
+}
+
+ompl::base::Cost ompl::base::ROPPathLengthOptimizationObjective::stateCost(const State *) const
+{
+    return identityCost();
+}
+
+ompl::base::Cost ompl::base::ROPPathLengthOptimizationObjective::motionCost(const State *s1, const State *s2) const
+{
+    return Cost(si_->distance(s1, s2));
+}
+
+ompl::base::Cost ompl::base::ROPPathLengthOptimizationObjective::motionCostHeuristic(const State *s1,
+                                                                                  const State *s2) const
+{
+    return motionCost(s1, s2);
+}
+
+ompl::base::InformedSamplerPtr ompl::base::ROPPathLengthOptimizationObjective::allocInformedStateSampler(
+    const ProblemDefinitionPtr &probDefn, unsigned int maxNumberCalls) const
+{
+// Make the direct path-length informed sampler and return. If OMPL was compiled with Eigen, a direct version is
+// available, if not a rejection-based technique can be used
+    return std::make_shared<PathLengthDirectInfSampler>(probDefn, maxNumberCalls);
+}
+
+ompl::base::ROPPathLengthOptimizationObjective::ROPPathLengthOptimizationObjective(const SpaceInformationPtr &si)
+  : ompl::base::OptimizationObjective(si)
+{
+    description_ = "ROPE Path Length";
+
+    // Setup a default cost-to-go heuristics:
+    setCostToGoHeuristic(base::goalRegionCostToGo);
+
+    name="rop metrics";
+
+    urdf::Model robo_model;
+    robo_model.initParam("robot_description");
+    std::string base_frame_ = "world";
+    std::string tool_frame = "tip";
+    if (!nh_.getParam("base_frame", base_frame_))
+    {
+        ROS_ERROR("%s/base_frame not defined", nh_.getNamespace().c_str());
+        throw std::invalid_argument("base_frame is not defined");
+    }
+    if (!nh_.getParam("tool_frame", tool_frame))
+    {
+        ROS_ERROR("%s/tool_frame not defined", nh_.getNamespace().c_str());
+        throw std::invalid_argument("base_frame is not defined");
+    }
+
+    // if (!nh_.getParam("avoid_prob_threshold", prob_threshold))
+    // {
+    //   ROS_WARN_STREAM("/avoid_prob_threshold not defined, using "<<prob_threshold);
+    // }
+    
+    if (!nh_.getParam("links_for_rop", links_to_check))
+    {
+        ROS_ERROR("%s/links_for_rop not defined", nh_.getNamespace().c_str());
+        throw std::invalid_argument("links_for_rop is not defined");
+    }  
+    if (!nh_.getParam("rop_resolution", m_resolution))
+    {
+        ROS_WARN_STREAM("/rop_resolution not defined, using "<<m_resolution);
+    }
+    
+    Eigen::Vector3d grav;
+    grav << 0, 0, -9.806;
+    chain = rosdyn::createChain(robo_model, base_frame_, tool_frame, grav);
+    
+    link_names=chain->getLinksName();
+    Eigen::VectorXd q;
+    q.resize(link_names.size());
+    q.setZero();
+    m_transformations=chain->getTransformations(q);
+    int n_pts = 0;
+    for (std::string link_name:links_to_check) {
+        int it = std::find(link_names.begin(),link_names.end(),link_name) - link_names.begin();
+        Eigen::Affine3d t_tip = m_transformations.at(it);
+        std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> link_pts;
+        link_pts.emplace_back(t_tip.translation());
+        if (it>0) {
+        Eigen::Affine3d t_start = m_transformations.at(it-1);
+        Eigen::Vector3d diff = t_tip.translation()-t_start.translation();
+        int npts = std::ceil(diff.norm()*m_inv_resolution);
+        for (int i=1;i<npts;i++) {
+            link_pts.emplace_back(t_start.translation()+double(i/npts)*diff);
+            n_pts++;
+        }
+        }
+        m_test_points.insert({link_name,link_pts});
+    }
+    m_points.resize(n_pts);
+
+    if (!nh_.getParam("computation_step", step_))
+    {
+        ROS_ERROR("%s/computation_step not defined", nh_.getNamespace().c_str());
+    }
+    std::string opv_topic = "rop_opvs";
+    if (!nh_.getParam("rop_opv_topic", opv_topic))
+    {
+        ROS_WARN_STREAM("/rop_opv_topic not defined, using "<<opv_topic);
+    }
+
+    WorkcellGrid();
+
+    sub_opvs = nh_.subscribe<rop_planning::opv_array_msg>(opv_topic,1,&ROPMetrics::opvs_callback,this);
+}
+
+void ompl::base::ROPPathLengthOptimizationObjective::WorkcellGrid(void)
+{
+  
+  std::vector<double> ws_lb_param;
+
+  if (nh_.getParam("workspace_lower_bounds_xyz",ws_lb_param))
+  {
+    for (int i=0;i<3;i++) workspace_lb[i] = ws_lb_param[i];
+  } else {
+    ROS_DEBUG("workspace_lower_bounds_xyz is not set, default={-1,-1,0.5}");
+  }  
+
+  std::vector<double> ws_ub_param;
+
+  if (nh_.getParam("workspace_upper_bounds_xyz",ws_ub_param))
+  {
+    for (int i=0;i<3;i++) workspace_ub[i] = ws_ub_param[i];
+  } else {
+    ROS_DEBUG("workspace_lower_bounds_xyz is not set, default={1,1,2.5}");
+  }
+  m_inv_resolution=1/m_resolution;
+  std::vector<int> npnt;
+  for (int i=0;i<3;i++) {
+    npnt.push_back(std::ceil((workspace_ub[i]-workspace_lb[i])*m_inv_resolution));
+    assert(npnt.back()>0);
+  }
+  // m_voxel_volume=m_resolution * m_resolution * m_resolution;
+  m_occupancy.resize(npnt[0],npnt[1],npnt[2]);
+  m_occupancy.setConstant(1.0f);
+  m_npnt=npnt;
+}
+
+void ompl::base::ROPPathLengthOptimizationObjective::opvs_callback(const rop_planning::opv_array_msg::ConstPtr& msg)
+{
+  m_occupancy.setConstant(1.0f);
+  for (rop_planning::opv_msg opv:msg->opv_array) {
+    //descritize OPV
+    Eigen::Tensor<double,3> occup(m_npnt[0],m_npnt[1],m_npnt[2]);
+    occup.setConstant(1.0f);
+    Eigen::Vector3d near_pt_1;
+    for (int i=0;i<3;i++) near_pt_1[i]=opv.near_pt_1[i];
+    Eigen::Vector3d far_pt_1;
+    for (int i=0;i<3;i++) far_pt_1[i]=opv.far_pt_1[i];
+    Eigen::Vector3d near_pt_2;
+    for (int i=0;i<3;i++) near_pt_2[i]=opv.near_pt_2[i];
+    Eigen::Vector3d far_pt_2;
+    for (int i=0;i<3;i++) far_pt_2[i]=opv.far_pt_2[i];
+
+    Eigen::Vector3d pts1_delta = far_pt_1-near_pt_1;
+    Eigen::Vector3d pts2_delta = far_pt_2-near_pt_2;
+    int npts1 = std::ceil(pts1_delta.norm()*m_inv_resolution);
+    int npts2 = std::ceil(pts2_delta.norm()*m_inv_resolution);
+    for (int i=0;i<=npts1;i++) {
+      Eigen::Vector3d p1 = near_pt_1+double(i/npts1)*pts1_delta;
+      for (int j=0;j<=npts2;j++) {
+        Eigen::Vector3d p2 = near_pt_2+double(j/npts2)*pts2_delta;
+        //descritize p1->p2 line
+        Eigen::Vector3d pts3_delta = p2-p1;
+        int npts3 = std::ceil(pts3_delta.norm()*m_inv_resolution);
+        for (int k=0;k<=npts3;k++)
+        {
+          Eigen::Vector3d p = p1+double(k/npts3)*pts3_delta;
+          double ix=(p(0)-workspace_lb(0))*m_inv_resolution;
+          double iy=(p(1)-workspace_lb(1))*m_inv_resolution;
+          double iz=(p(2)-workspace_lb(2))*m_inv_resolution;
+
+          if ( (ix<0) || (ix>=m_npnt[0]))
+            continue;
+          if ( (iy<0) || (iy>=m_npnt[1]))
+            continue;
+          if ( (iz<0) || (iz>=m_npnt[2]))
+            continue;
+
+          occup(int(std::floor(ix)),
+              int(std::floor(iy)),
+              int(std::floor(iz)))=opv.prob_successful_passage;
+          occup(int(std::floor(ix)),
+              int(std::floor(iy)),
+              int(std::ceil(iz)))=opv.prob_successful_passage;
+          occup(int(std::floor(ix)),
+              int(std::ceil(iy)),
+              int(std::floor(iz)))=opv.prob_successful_passage;
+          occup(int(std::floor(ix)),
+              int(std::ceil(iy)),
+              int(std::ceil(iz)))=opv.prob_successful_passage;
+          occup(int(std::ceil(ix)),
+              int(std::floor(iy)),
+              int(std::floor(iz)))=opv.prob_successful_passage;
+          occup(int(std::ceil(ix)),
+              int(std::floor(iy)),
+              int(std::ceil(iz)))=opv.prob_successful_passage;
+          occup(int(std::ceil(ix)),
+              int(std::ceil(iy)),
+              int(std::floor(iz)))=opv.prob_successful_passage;
+          occup(int(std::ceil(ix)),
+              int(std::ceil(iy)),
+              int(std::ceil(iz)))=opv.prob_successful_passage;
+        }
+      }
+    }
+    m_occupancy=(m_occupancy*occup);
+  }
+  // m_occupancy = 1/m_occupancy;
+
+}
+
+double ompl::base::ROPPathLengthOptimizationObjective::totalROP(const std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> >& points)
+{
+  double total_rop=1.0;
+  double ix;
+  double iy;
+  double iz;
+
+
+  for (const Eigen::Vector3d& p: points)
+  {
+    ix=(p(0)-workspace_lb(0))*m_inv_resolution;
+    if ( (ix<0) || (ix>=m_npnt[0]))
+      continue;
+
+    iy=(p(1)-workspace_lb(1))*m_inv_resolution;
+    if ( (iy<0) || (iy>=m_npnt[1]))
+      continue;
+
+    iz=(p(2)-workspace_lb(2))*m_inv_resolution;
+    if ( (iz<0) || (iz>=m_npnt[2]))
+      continue;
+
+    double prob_success=            m_occupancy(int(std::floor(ix)),int(std::floor(iy)),int(std::floor(iz) ));
+    prob_success=std::min(prob_success,m_occupancy(int(std::floor(ix)),int(std::floor(iy)),int(std::ceil(iz)  )));
+    prob_success=std::min(prob_success,m_occupancy(int(std::floor(ix)),int(std::ceil(iy) ),int(std::floor(iz) )));
+    prob_success=std::min(prob_success,m_occupancy(int(std::floor(ix)),int(std::ceil(iy) ),int(std::ceil(iz)  )));
+    prob_success=std::min(prob_success,m_occupancy(int(std::ceil(ix) ),int(std::floor(iy)),int(std::floor(iz) )));
+    prob_success=std::min(prob_success,m_occupancy(int(std::ceil(ix) ),int(std::floor(iy)),int(std::ceil(iz)  )));
+    prob_success=std::min(prob_success,m_occupancy(int(std::ceil(ix) ),int(std::ceil(iy) ),int(std::floor(iz) )));
+    prob_success=std::min(prob_success,m_occupancy(int(std::ceil(ix) ),int(std::ceil(iy) ),int(std::ceil(iz)  )));
+    total_rop*=prob_success;
+  }
+  // total_occupancy*=m_voxel_volume;
+  return 1/total_rop;
+}
+
+
+double ompl::base::ROPPathLengthOptimizationObjective::occupiedROPMultiplier(const Eigen::VectorXd &q)
+{
+  unsigned ipnt=0;
+  m_transformations=chain->getTransformations(q);
+  for (std::string link_name:links_to_check) {
+    auto il = std::find(link_names.begin(),link_names.end(),link_name);
+    if ( il != link_names.end() )
+    {
+      int it = il-link_names.begin();
+      const Eigen::Affine3d& t= m_transformations.at(it);
+      const auto& pnts= m_test_points.at(link_name);
+      for (unsigned int ip=0;ip<pnts.size();ip++)
+      {
+        m_points.at(ipnt++)=t*pnts.at(ip);
+      }
+    }
+  }
+  return totalROP(m_points);
+}
+
+ompl::base::Cost ompl::base::ROPPathLengthOptimizationObjective::motionCost(const State *state1, const State *state1) const
+{
+    const auto *cstate1 = state1->as<StateType>();
+    const auto *cstate2 = state2->as<StateType>();
+
+    const double *s1 = static_cast<const StateType *>(state1)->values;
+    const double *s2 = static_cast<const StateType *>(state2)->values;
+
+    int dimension_ = state1->getDimension();
+    Eigen::VectorXd p(dimension_);
+    Eigen::VectorXd c(dimension_);
+    for (unsigned int i = 0; i < dimension_; ++i)
+    {   
+        p[i] = s1++;
+        c[i] = s2++;
+    }
+    return Cost(rop_cost(p,c));
+}
+
+double ompl::base::ROPPathLengthOptimizationObjective::rop_cost(const Eigen::VectorXd& parent, const Eigen::VectorXd& new_node)
+{    
+  double length = (new_node - parent).norm();
+  if (length < 1e-6)
+    return length;
+
+  double cost = 0.0;
+  unsigned int nsteps = std::ceil(length / step_);
+  double inv_nsteps = 1.0 / nsteps;
+  double distance = length / nsteps;
+  Eigen::VectorXd diff = (new_node - parent)* inv_nsteps;
+  for (unsigned int istep = 0; istep <= nsteps; istep++)
+  {
+    Eigen::VectorXd q = parent + diff * istep;
+    //check for intersection of voxels
+    cost += distance*occupiedROPMultiplier(q);
+  }
+
+  return cost;
+}
+
+
